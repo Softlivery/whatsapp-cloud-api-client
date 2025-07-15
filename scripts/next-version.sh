faa2efe689b5a6c4e7a5f8bccfa2bf103e38aa9f
Matias <tebo02ar@gmail.com>
2025-07-15 21:26:12 +0200

    Add version file

--- scripts/next-version.sh
+++ scripts/next-version.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-LATEST_TAG=$(git tag --list 'v*' --sort=-creatordate | head -n 1)
-VERSION=${LATEST_TAG#v}
 
+VERSION_FILE="VERSION"
+
+if [[ ! -f "$VERSION_FILE" ]]; then
+  echo "0.0.0" > "$VERSION_FILE"
+fi
+
+VERSION=$(cat "$VERSION_FILE")
 IFS='.' read -r -a parts <<< "$VERSION"
+
+# Ensure we have 3 version parts
+while [ "${#parts[@]}" -lt 3 ]; do
+  parts+=("0")
+done
+
+# Increment PATCH version
 parts[2]=$((parts[2]+1))
-echo "${parts[0]}.${parts[1]}.${parts[2]}"
+
+NEW_VERSION="${parts[0]}.${parts[1]}.${parts[2]}"
+
+# Output version and write to file
+echo "$NEW_VERSION" > "$VERSION_FILE"
+echo "$NEW_VERSION"
