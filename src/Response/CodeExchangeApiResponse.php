3d4c6901168dc93cdfd28d83534bc68e5bd3c3a2
Matias <tebo02ar@gmail.com>
2025-08-31 21:34:02 +0200

    FIX CodeExchangeApiResponse

--- src/Response/CodeExchangeApiResponse.php
+++ src/Response/CodeExchangeApiResponse.php
@@ -2,18 +2,8 @@
 
 namespace Softlivery\WhatsappCloudApiClient\Response;
 
-use Softlivery\WhatsappCloudApiClient\Http\HttpResponse;
-
 class CodeExchangeApiResponse extends ApiResponse
 {
-
-    private HttpResponse $httpResponse;
-
-    public function __construct(HttpResponse $response)
-    {
-        $this->httpResponse = $response;
-    }
-
     public function getAccessToken(): string
     {
         return $this->httpResponse->getDecodedBody()["access_token"];
